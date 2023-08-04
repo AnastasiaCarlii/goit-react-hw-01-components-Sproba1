235473f9717f33ca65f928e2c9515e72742c1b14
Anastasia Carlii <nastjaorosszul@gmail.com>
2023-08-04 02:12:08 +0100

    task n1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
-export const App = () => {
+import { Profile } from './Profile/Profile';
+import user from './user.json';
+
+export const App = ({ children }) => {
   return (
     <div
       style={{
-        height: '100vh',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      {children}
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        likes={user.stats.likes}
+        views={user.stats.views}
+      />
     </div>
   );
 };
