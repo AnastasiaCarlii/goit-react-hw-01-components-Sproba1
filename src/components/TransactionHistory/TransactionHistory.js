e666a868e8cb31847e3460b054c89b4191dcd34b
Anastasia Carlii <nastjaorosszul@gmail.com>
2023-08-06 22:46:35 +0100

    some changes

--- src/components/TransactionHistory/TransactionHistory.js
+++ src/components/TransactionHistory/TransactionHistory.js
@@ -1,32 +1,42 @@
 import PropTypes from 'prop-types';
 import {
-  Container,
-  TransactionList,
-} from 'components/TransactionHistory/TransactionHistory.styled';
-import { TransactionTitle } from 'components/TransactionHistory/TransactionTitle/TransactionTitle';
-import { TransactionListItem } from 'components/TransactionHistory/TransactionListItem/TransactionListItem';
+  Table,
+  Tbody,
+  Thead,
+  TableTH,
+  TableTD,
+  TableRow,
+} from './TransactionHistory.styled';
 
 export const TransactionHistory = ({ items }) => {
   return (
-    <Container>
-      <TransactionTitle />
-      <TransactionList>
-        {items.map(({ id, type, amount, currency }) => (
-          <TransactionListItem
-            key={id}
-            type={type}
-            amount={amount}
-            currency={currency}
-          />
-        ))}
-      </TransactionList>
-    </Container>
+    <Table>
+      <Thead>
+        <TableRow>
+          <TableTH>Type</TableTH>
+          <TableTH>Amount</TableTH>
+          <TableTH>Currency</TableTH>
+        </TableRow>
+      </Thead>
+      {items.map(item => (
+        <Tbody key={item.id}>
+          <TableRow type={item.type}>
+            <TableTD>{item.type}</TableTD>
+            <TableTD>{item.amount}</TableTD>
+            <TableTD>{item.currency}</TableTD>
+          </TableRow>
+        </Tbody>
+      ))}
+    </Table>
   );
 };
 
 TransactionHistory.propTypes = {
   items: PropTypes.arrayOf(
     PropTypes.shape({
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
       id: PropTypes.string.isRequired,
     })
   ),
